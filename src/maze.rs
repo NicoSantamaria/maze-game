ce51d289e76c19556d781a9cb72e2d2c2e87a38f
NicoSantamaria <nicoasantamaria@gmail.com>
2024-09-29 13:11:44 -0700

    abstract draw_pizel for use by game_loop

--- src/maze.rs
+++ src/maze.rs
@@ -21,7 +21,7 @@ pub const DIMENSION: usize = 11;
 pub const MAZE: [[MazeTypes; DIMENSION]; DIMENSION] = [
     [Wall,Strt,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall],
     [Wall,None,Wall,None,Wall,None,Wall,None,Wall,None,Wall],
-    [Wall,None,Wall,None,Wall,None,None,None,Wall,None,Wall],
+    [Wall,None,Wall,None,Wall,Enem,None,None,Wall,None,Wall],
     [Wall,None,Wall,None,Wall,None,Wall,None,Wall,None,Wall],
     [Wall,None,None,None,None,None,Wall,None,None,None,Wall],
     [Wall,Wall,None,Wall,None,Wall,Wall,Wall,Wall,Wall,Wall],
@@ -32,10 +32,15 @@ pub const MAZE: [[MazeTypes; DIMENSION]; DIMENSION] = [
     [Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Ends,Wall],
 ];
 
-pub fn draw_pixel(stdout: &mut io::Stdout, x_pos: usize, y_pos: usize) -> io::Result<()> {
+pub fn draw_pixel(
+    stdout: &mut io::Stdout,
+    maze: [[MazeTypes; DIMENSION]; DIMENSION], 
+    x_pos: usize, 
+    y_pos: usize
+) -> io::Result<()> {
     let x: u16 = x_pos as u16;
     let y: u16 = y_pos as u16;
-    let color = match MAZE[x_pos][y_pos] {
+    let color = match maze[x_pos][y_pos] {
         Strt => Color::Green,
         Ends => Color::Red,
         Wall => Color::White,
@@ -46,7 +51,8 @@ pub fn draw_pixel(stdout: &mut io::Stdout, x_pos: usize, y_pos: usize) -> io::Re
 
     stdout
         .queue(cursor::MoveTo(x, y))?
-        .queue(style::PrintStyledContent("█".with(color)))?;
+        .queue(style::PrintStyledContent("█".with(color)))?
+        .queue(cursor::MoveTo(0, 0))?;
 
     Ok(())
 }
@@ -56,7 +62,7 @@ pub fn draw_maze(stdout: &mut io::Stdout) -> io::Result<()> {
 
     for x in 0..DIMENSION {
         for y in 0..DIMENSION {
-            let _ = draw_pixel(stdout, x, y);
+            let _ = draw_pixel(stdout, MAZE, x, y);
         }
     }
 
